17b97ba00f2859ce9ffda413473ba713925db021
xu_yanfeng <xu_yanfeng@126.com>
2023-07-05 17:58:56 +0800

    使用vue编写webview panel

---
+++ webview/src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+
+  name: "main",
+  setup(props, ctx) {
+    return {};
+  },
+})
+</script>
+
+<template>
+  <div>22</div>
+</template>
+
+<style scoped></style>
